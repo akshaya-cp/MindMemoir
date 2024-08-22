cda52a2d75228c01085a54b6c7238688a3bf06b3
akshaya-cp <akshayasingh61@gmail.com>
2024-08-23 00:43:32 +0530

    3

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,41 +1,51 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { Suspense } from 'react'
+import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
 
 const UpdatePrompt = () => {
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get("id");
-
   const [post, setPost] = useState({ prompt: "", tag: "" });
   const [submitting, setIsSubmitting] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [promptId, setPromptId] = useState(null);
 
   useEffect(() => {
-    const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
-      const data = await response.json();
+    // Use the useEffect to set promptId on the client-side
+    const searchParams = new URLSearchParams(window.location.search);
+    const id = searchParams.get("id");
+    setPromptId(id);
 
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
+    if (id) {
+      const getPromptDetails = async () => {
+        const response = await fetch(`/api/prompt/${id}`);
+        const data = await response.json();
 
+        setPost({
+          prompt: data.prompt,
+          tag: data.tag,
+        });
+
+        setLoading(false);
+      };
+
+      getPromptDetails();
+    } else {
       setLoading(false);
-    };
-
-    if (promptId) getPromptDetails();
-  }, [promptId]);
+    }
+  }, []);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
 
-    if (!promptId) return alert("Missing PromptId!");
+    if (!promptId) {
+      alert("Missing PromptId!");
+      setIsSubmitting(false);
+      return;
+    }
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
@@ -59,16 +69,13 @@ const UpdatePrompt = () => {
   if (loading) return <div>Loading...</div>;
 
   return (
-    <Suspense>
-      
-      <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-      />
-    </Suspense>
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
   );
 };
 
